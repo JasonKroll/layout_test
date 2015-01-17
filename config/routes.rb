9754356f3fd7f6f6715cb9448b65a7d12a3a0901
Jason Kroll <Krolly@gmail.com>
2015-01-17 14:13:58 +1000

    upated layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static#home'
 
-  devise_for :users controllers: {
-    sesseions: 'sessions'
-  }
+  # devise_for :users, controllers: {
+  #   sessions: 'sessions'
+  # }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
